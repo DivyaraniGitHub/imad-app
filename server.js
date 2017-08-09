79f071cbf8f541bf12e40d037b1367d944a8e04d
DivyaraniGitHub <30767498+DivyaraniGitHub@users.noreply.github.com>
2017-08-09 22:14:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/https://upload.wikimedia.org/wikipedia/commons/b/b2/Download_Files_4_You_Logo.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Download_Files_4_You_Logo.png'));
 });
 
 
